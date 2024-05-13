6c24ba6cc74de576565d8cf1735dc5c99c10743a
EverGaster <98375412+EverGasterXd@users.noreply.github.com>
2024-05-13 12:57:22 -0600

    borrando mi numero por pendejo

--- proyecto/obj/Debug/net8.0/proyecto.AssemblyInfo.cs
+++ proyecto/obj/Debug/net8.0/proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d963419c0a84dd5c1828ddd2a2394887a92e035d")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
